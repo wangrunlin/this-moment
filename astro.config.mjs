d983f41bffecb42e2b5e63842393c1c5b794c673
Leo Wang <me@alin.run>
2024-10-21 14:07:54 +0800

    feat: update cloudflare adapter and upgrade upstream packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
+
+  adapter: cloudflare(),
 });
